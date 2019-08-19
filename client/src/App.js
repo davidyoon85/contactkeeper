cc85dd33d9036221e728ccbffe7d9322666bcd90
David Yoon <davidyoon85@gmail.com>
2019-08-19 16:12:39 -0400

    set frontend context

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
-import axios from "axios";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import ContactState from "./context/contact/ContactState";
+
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 const App = () => {
-  axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
-    console.log(res.data);
-  });
-  return <div className="App" />;
+  return (
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
+  );
 };
 
 export default App;
