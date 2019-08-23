1c660ecd5c640508679d5488a149431d79451b3c
David Yoon <davidyoon85@gmail.com>
2019-08-22 20:38:28 -0400

    connect react with express

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFiler from "../contacts/ContactFiler";
 
 const Home = () => {
   return (
     <div className="grid-2">
-      <div>{/* form */}</div>
       <div>
+        <ContactForm />
+      </div>
+      <div>
+        <ContactFiler />
         <Contacts />
       </div>
     </div>
